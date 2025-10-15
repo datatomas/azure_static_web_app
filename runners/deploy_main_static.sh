2eabc7ae47e21114ea9e127e105ac9b596b71321
Tomas Suarez <83978153+datatomas@users.noreply.github.com>
2025-10-14 19:34:04 -0500

    Update deploy_main_static.sh
    feature:modified to take variabels from wsf env and added same env as the rest of the files.

--- runners/deploy_main_static.sh
+++ runners/deploy_main_static.sh
@@ -5,20 +5,21 @@
 
 set -e
 
-RG_NAME="RG_PUEBLITO_STATIC_PRD"
-LOCATION="mexicocentral"
+: "${RG:?Set RG in ~/.env (resource group)}"
+: "${REGION:?Set REGION in ~/.env (e.g. mexicocentral)}"
+
 
 echo "🔹 Checking Azure login..."
 az account show >/dev/null 2>&1 || az login
 
 echo "🔹 Ensuring resource group exists..."
-az group create --name "$RG_NAME" --location "$LOCATION"
+az group create --name "$RG" --location "$REGION"
 
 echo "🚀 Deploying Bicep template..."
 az deployment group create \
   --name deployStaticWebApp \
-  --resource-group "$RG_NAME" \
-  --template-file main.bicep \
-  --parameters @parameters.json
+  --resource-group "$RG" \
+  --template-file static_storage_afd.bicep \
+  --parameters @static_storage_afd_params.json
 
 echo "✅ Deployment completed successfully."
